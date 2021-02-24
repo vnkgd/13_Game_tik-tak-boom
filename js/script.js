9b9d1100bbb21a1e019c1a4b94b335cec5e152db
Val <vnkgd@mail.ru>
2021-02-25 01:25:43 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+window.onload = function()
+{
+    tikTakBoom.init(
+        tasks,
+        document.getElementById('timerField'),
+        document.getElementById('gameStatusField'),
+        document.getElementById('questionField'),
+        document.getElementById('answer1'),
+        document.getElementById('answer2'),
+    )
+    tikTakBoom.run();
+}
+
+
---
+++ js/tikTakBoomTasks.js
@@ -0,0 +1,25 @@
+const tasks = `
+
+    [
+        {
+            "question": "2 * 2 = ",
+            "answer1": { "result": true, "value": "4" },
+            "answer2": { "result": false, "value": "3" }
+        },
+        {
+            "question": "Как зовут крокодила, лучшего друга Чебурашки?",
+            "answer1": { "result": false, "value": "Данди" },
+            "answer2": { "result": true, "value": "Гена" }
+        },
+        {
+            "question": "5 + 5",
+            "answer1": { "result": false, "value": "1" },
+            "answer2": { "result": true, "value": "10" }
+        },
+        {
+            "question": "Столица Италии?",
+            "answer1": { "result": false, "value": "Ватикан" },
+            "answer2": { "result": true, "value": "Рим" }
+        }
+    ]
+`;
