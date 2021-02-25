48e47d639a0acee437add8c31d53beba2b7ddfa0
Val <vnkgd@mail.ru>
2021-02-25 11:39:32 +0200

    val

---
+++ js/questions.js
@@ -0,0 +1,19 @@
+const JSONQuestions =`
+[
+    {
+        "question": "Где находится Никарагуа?",
+        "answer1": { "result": true, "value": "Центральная Америка" },
+        "answer2": { "result": false, "value": "Центральная Африка" },
+        "answer3": { "result": false, "value": "Южная Африка" },
+        "answer4": { "result": false, "value": "Австралия" },
+        "answer5": { "result": false, "value": "Южная Америка" }
+    },
+    {
+        "question": "Какой частью Волшебной страны правила Гингема?",
+        "answer1": { "result": true, "value": "Голубой" },
+        "answer2": { "result": false, "value": "Розовой" },
+        "answer3": { "result": false, "value": "Желтой" },
+        "answer4": { "result": false, "value": "Фиолетовой" },
+        "answer5": { "result": false, "value": "Изумрудной" }
+    },
+]`
--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
+
+//фукнция, которая сработает при загрузке страницы
 window.onload = function()
 {
     tikTakBoom.init(
         tasks,
-        document.getElementById('timerField'),
+        document.getElementById('timerField'),//
         document.getElementById('gameStatusField'),
         document.getElementById('questionField'),
         document.getElementById('answer1'),
